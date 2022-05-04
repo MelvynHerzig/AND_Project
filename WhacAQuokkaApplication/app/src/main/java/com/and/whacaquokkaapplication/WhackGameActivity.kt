23cc1e518404339bd532729873236820ea7a3d85
Berney Alec <beral@sevjnet.ch>
2022-05-04 09:21:26 +0200

    adding image and first version of home page

---
+++ WhacAQuokkaApplication/app/src/main/java/com/and/whacaquokkaapplication/WhackGameActivity.kt
@@ -0,0 +1,11 @@
+package com.and.whacaquokkaapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WhackGameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_whack_game)
+    }
+}
