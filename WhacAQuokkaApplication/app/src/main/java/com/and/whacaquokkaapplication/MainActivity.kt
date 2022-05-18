233450217b4acefb7a23a91cdfdc0f8d88521db8
Berney Alec <beral@sevjnet.ch>
2022-05-18 09:33:06 +0200

    main activity linked

--- WhacAQuokkaApplication/app/src/main/java/com/and/whacaquokkaapplication/MainActivity.kt
+++ WhacAQuokkaApplication/app/src/main/java/com/and/whacaquokkaapplication/MainActivity.kt
@@ -2,10 +2,26 @@ package com.and.whacaquokkaapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var discoverButton : Button
+    private lateinit var advertButton : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        discoverButton = findViewById(R.id.discover_button)
+        advertButton = findViewById(R.id.advert_button)
+
+        discoverButton.setOnClickListener {
+            // TODO
+        }
+
+        advertButton.setOnClickListener {
+            // TODO
+        }
     }
 }
