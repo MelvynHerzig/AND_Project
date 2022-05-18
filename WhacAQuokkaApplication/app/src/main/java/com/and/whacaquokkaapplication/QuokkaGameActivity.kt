94beca3200df9b7f7743afe47224c2df0d3ad727
Berney Alec <beral@sevjnet.ch>
2022-05-18 09:53:21 +0200

    whack and game part linked

--- WhacAQuokkaApplication/app/src/main/java/com/and/whacaquokkaapplication/QuokkaGameActivity.kt
+++ WhacAQuokkaApplication/app/src/main/java/com/and/whacaquokkaapplication/QuokkaGameActivity.kt
@@ -2,10 +2,88 @@ package com.and.whacaquokkaapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
 class QuokkaGameActivity : AppCompatActivity() {
+
+    private lateinit var scoreTextView : TextView
+    private lateinit var timeTextView : TextView
+
+    private lateinit var quitButton: Button
+
+    private lateinit var quokka1 : ImageView
+    private lateinit var quokka2 : ImageView
+    private lateinit var quokka3 : ImageView
+    private lateinit var quokka4 : ImageView
+    private lateinit var quokka5 : ImageView
+    private lateinit var quokka6 : ImageView
+    private lateinit var quokka7 : ImageView
+    private lateinit var quokka8 : ImageView
+    private lateinit var quokka9 : ImageView
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quokka_game)
+
+        scoreTextView = findViewById(R.id.score)
+        timeTextView = findViewById(R.id.time)
+
+        quitButton = findViewById(R.id.quit_image_button)
+
+        quokka1 = findViewById(R.id.quokka_1)
+        quokka2 = findViewById(R.id.quokka_2)
+        quokka3 = findViewById(R.id.quokka_3)
+        quokka4 = findViewById(R.id.quokka_4)
+        quokka5 = findViewById(R.id.quokka_5)
+        quokka6 = findViewById(R.id.quokka_6)
+        quokka7 = findViewById(R.id.quokka_7)
+        quokka8 = findViewById(R.id.quokka_8)
+        quokka9 = findViewById(R.id.quokka_9)
+
+        quitButton.setOnClickListener {
+            finish()
+        }
+
+        quokka1.setOnClickListener {
+            quokka1.setImageResource(R.drawable.quokka)
+            // TODO
+        }
+
+        quokka2.setOnClickListener {
+            quokka2.setImageResource(R.drawable.quokka)
+            // TODO
+        }
+
+        quokka3.setOnClickListener {
+            quokka3.setImageResource(R.drawable.quokka)
+            // TODO
+        }
+
+        quokka4.setOnClickListener {
+            quokka4.setImageResource(R.drawable.quokka)
+            // TODO
+        }
+
+        quokka5.setOnClickListener {
+            quokka5.setImageResource(R.drawable.quokka)
+            // TODO
+        }
+
+        quokka6.setOnClickListener {
+            quokka7.setImageResource(R.drawable.quokka)
+            // TODO
+        }
+
+        quokka8.setOnClickListener {
+            quokka8.setImageResource(R.drawable.quokka)
+            // TODO
+        }
+
+        quokka9.setOnClickListener {
+            quokka9.setImageResource(R.drawable.quokka)
+            // TODO
+        }
     }
 }
--- WhacAQuokkaApplication/app/src/main/java/com/and/whacaquokkaapplication/WhackGameActivity.kt
+++ WhacAQuokkaApplication/app/src/main/java/com/and/whacaquokkaapplication/WhackGameActivity.kt
@@ -2,23 +2,88 @@ package com.and.whacaquokkaapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
 class WhackGameActivity : AppCompatActivity() {
+
+    private lateinit var scoreTextView : TextView
+    private lateinit var timeTextView : TextView
+
+    private lateinit var quitButton: Button
+
+    private lateinit var hole1 : ImageView
+    private lateinit var hole2 : ImageView
+    private lateinit var hole3 : ImageView
+    private lateinit var hole4 : ImageView
+    private lateinit var hole5 : ImageView
+    private lateinit var hole6 : ImageView
+    private lateinit var hole7 : ImageView
+    private lateinit var hole8 : ImageView
+    private lateinit var hole9 : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_whack_game)
-    }
 
-    /*final ImageView image = (ImageView) view.findViewById(R.id.fav);
+        scoreTextView = findViewById(R.id.score)
+        timeTextView = findViewById(R.id.time)
 
-    image.setOnClickListener(new View.OnClickListener(){
+        quitButton = findViewById(R.id.quit_image_button)
 
-        @Override
-        public void onClick(View v){
-            image.setBackgroundResource(R.drawable.ic_fav);
+        hole1 = findViewById(R.id.hole_1)
+        hole2 = findViewById(R.id.hole_2)
+        hole3 = findViewById(R.id.hole_3)
+        hole4 = findViewById(R.id.hole_4)
+        hole5 = findViewById(R.id.hole_5)
+        hole6 = findViewById(R.id.hole_6)
+        hole7 = findViewById(R.id.hole_7)
+        hole8 = findViewById(R.id.hole_8)
+        hole9 = findViewById(R.id.hole_9)
 
-            final Button btn = (Button) v.findViewById(R.id.fav);
-            btn.setPressed(true);
+        quitButton.setOnClickListener {
+            finish()
         }
-    });*/
+
+        hole1.setOnClickListener {
+            hole1.setImageResource(R.drawable.hole)
+            // TODO
+        }
+
+        hole2.setOnClickListener {
+            hole2.setImageResource(R.drawable.hole)
+            // TODO
+        }
+
+        hole3.setOnClickListener {
+            hole3.setImageResource(R.drawable.hole)
+            // TODO
+        }
+
+        hole4.setOnClickListener {
+            hole4.setImageResource(R.drawable.hole)
+            // TODO
+        }
+
+        hole5.setOnClickListener {
+            hole5.setImageResource(R.drawable.hole)
+            // TODO
+        }
+
+        hole6.setOnClickListener {
+            hole7.setImageResource(R.drawable.hole)
+            // TODO
+        }
+
+        hole8.setOnClickListener {
+            hole8.setImageResource(R.drawable.hole)
+            // TODO
+        }
+
+        hole9.setOnClickListener {
+            hole9.setImageResource(R.drawable.hole)
+            // TODO
+        }
+    }
 }
