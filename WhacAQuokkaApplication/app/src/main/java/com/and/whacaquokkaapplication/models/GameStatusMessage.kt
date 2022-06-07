b25f905135707908d96bb3f93778973b32223cd9
Berney Alec <beral@sevjnet.ch>
2022-06-07 16:35:16 +0200

    add generic message class

--- WhacAQuokkaApplication/app/src/main/java/com/and/whacaquokkaapplication/models/GameStatusMessage.kt
+++ WhacAQuokkaApplication/app/src/main/java/com/and/whacaquokkaapplication/models/GameStatusMessage.kt
@@ -2,7 +2,7 @@ package com.and.whacaquokkaapplication.models
 
 import kotlinx.serialization.Serializable
 
-// TO use:
+// To use:
 // https://kotlinlang.org/docs/serialization.html#example-json-serialization
 // val json = Json.encodeToString(Data(42, "str"))
 // val obj = Json.decodeFromString<Data>("""{"a":42, "b": "str"}""")
@@ -11,14 +11,10 @@ import kotlinx.serialization.Serializable
 // test sendpayload line 173
 
 /**
- * Enum class that represent a game status used to communicate
+ * Class that represent a game status
  * @author Berney Alec
  * @author Forestier Quentin
  * @author Herzig Melvyn
  */
 @Serializable
-enum class GameStatusMessage {
-    START,
-    STOP,
-    PAUSE
-}
+class GameStatusMessage(val status: GameStatus) : Message(MessageType.GameStatus) {}
--- WhacAQuokkaApplication/app/src/main/java/com/and/whacaquokkaapplication/models/ScoreStatusMessage.kt
+++ WhacAQuokkaApplication/app/src/main/java/com/and/whacaquokkaapplication/models/ScoreStatusMessage.kt
@@ -3,14 +3,14 @@ package com.and.whacaquokkaapplication.models
 import kotlinx.serialization.Serializable
 
 /**
- * Data class that represent a score status from game used to communicate
+ * Class that represent a score status from game used to communicate
  * @author Berney Alec
  * @author Forestier Quentin
  * @author Herzig Melvyn
  */
 @Serializable
-data class ScoreStatusMessage (
+class ScoreStatusMessage (
     val touched: Boolean,
     val quokkaScore: Int,
     val whackScore: Int
-)
+) : Message(MessageType.GameStatus) {}
